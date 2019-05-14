cf3b11278eff974b03bf2836212d633925fc5d9f
Ben Sooraj <bensoorajm@gmail.com>
2019-05-15 01:17:27 +0530

    2. Basic React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-
+import Router from './routers/Router'
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Online Shopping Cart</h1>        
+        <h1>Online Shopping Cart</h1>
+
+        <Router />
       </div>
     )
   }
