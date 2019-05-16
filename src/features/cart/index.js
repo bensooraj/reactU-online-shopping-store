5202ab0cd40f2bf0b9d229034b918586e028adc8
Ben Sooraj <bensoorajm@gmail.com>
2019-05-17 01:15:59 +0530

    Cart page is also implemented

--- src/features/cart/index.js
+++ src/features/cart/index.js
@@ -1,19 +1,80 @@
-import React from 'react'
+ import React from 'react'
 import {
     connect
 } from 'react-redux'
 
-
-export const cartItemsWithQuantity = (cartItems) => {
-    return cartItems.reduce((acc, item) => {
-
-        const filtereddItem = acc.filter(item2 => item2.id === item.id)[0];
-        filtereddItem !== undefined ? filtereddItem.quantity++
-            :
-            acc.push({
-                ...item,
-                quantity: 1
-            })
-        return acc;
-    }, [])
+function sort(items) {
+    return items.sort((a, b) => a.id - b.id )
 }
+
+function Cart(props) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Item</th>
+                    <th>Quantity</th>
+                    <th>+</th>
+                    <th>-</th>
+                    <th>Remove All</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    sort(props.cart).map(item => {
+                        return (
+                            <tr>
+                                <td>{item.name}</td>
+                                <td>{item.quantity}</td>
+                                <td>
+                                    <button
+                                        onClick={() => props.addToCart(item)}
+                                    >+</button>
+                                </td>
+                                <td>
+                                    <button
+                                        onClick={() => props.removeFromCart(item)}
+                                    >-</button>
+                                </td>
+                                <td>
+                                    <button
+                                        onClick={() => props.removeAllFromCart(item)}
+                                    >Remove All</button>
+                                </td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
+    )
+}
+
+const mapStateToProps = (state) => ({
+    cart: state.cart
+})
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addToCart: (item) => {
+            dispatch({
+                type: 'ADD',
+                payload: item
+            })
+        },
+        removeFromCart: (item) => {
+            dispatch({
+                type: 'REMOVE',
+                payload: item
+            })
+        },
+        removeAllFromCart: (item) => {
+            dispatch({
+                type: 'REMOVE_ALL',
+                payload: item
+            })
+        },
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
